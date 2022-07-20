bc74d327ff39eb1318864c03fe25342644082117
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-07-19 21:19:11 -0300

    Popup Reservation Done

--- src/food.js
+++ src/food.js
@@ -130,7 +130,7 @@ function addPopupReservation(){
 
         const formTitle = document.createElement("h3");
         formTitle.classList.add("formTitle");
-        formTitle.textContent = "Leave your info and get called back shortly!";
+        formTitle.textContent = "Leave your info and get a reply shortly!";
         popup.appendChild(formTitle);
 
         const buttonCancel = document.createElement("button");
@@ -140,7 +140,7 @@ function addPopupReservation(){
         buttonCancel.textContent = "X";
         popup.appendChild(buttonCancel);
         buttonCancel.addEventListener("click", () =>{
-            htmlContainer.removeChild(popup);
+            bg.removeChild(popup);
         });
 
         const formContainer = document.createElement("form");
@@ -180,7 +180,7 @@ function addPopupReservation(){
         inputPhone.classList.add("input")
         inputPhone.id = "phone";
         inputPhone.name = "phone";
-        inputPhone.type="text";
+        inputPhone.type="tel";
         //inputAuthor.value="";
         inputPhone.placeholder="1234-5678";
         divBlockTwo.appendChild(inputPhone);
@@ -198,7 +198,7 @@ function addPopupReservation(){
         inputMail.classList.add("input")
         inputMail.id = "mail";
         inputMail.name = "mail";
-        inputMail.type="mail";
+        inputMail.type="email";
         //inputPages.value="";
         inputMail.placeholder="paulthebeatle@yahoo.com";
         divBlockThree.appendChild(inputMail);
@@ -214,7 +214,7 @@ function addPopupReservation(){
 
         const labelTwo = document.createElement("label");
         labelTwo.htmlFor = "group";
-        labelTwo.textContent = "2 very cool people";
+        labelTwo.textContent = "2";
         divBlockFour.appendChild(labelTwo);
 
         const radioTwo = document.createElement("input");
@@ -228,12 +228,12 @@ function addPopupReservation(){
 
         const labelFour = document.createElement("label");
         labelFour.htmlFor = "group";
-        labelFour.textContent = "4 even cooler people";
+        labelFour.textContent = "4";
         divBlockFour.appendChild(labelFour);
 
         const radioFour = document.createElement("input");
         radioFour.classList.add("input")
-        radioFour.id = "four";
+        /*radioFour.id = "four";*/
         radioFour.name = "group";
         radioFour.type="radio";
         radioFour.value="4";
@@ -252,7 +252,7 @@ function addPopupReservation(){
         inputDate.classList.add("input")
         inputDate.id = "date";
         inputDate.name = "date";
-        inputDate.type="datetime-local";
+        inputDate.type="date";
         //inputImage.value="";
         let date = new Date().toLocaleDateString();
         inputDate.min= date;
@@ -271,21 +271,23 @@ function addPopupReservation(){
         selectTime.classList.add("input")
         selectTime.id = "time";
         selectTime.name = "time";
-        selectTime.placeholder="21:00";
+        selectTime.placeholder="Select Turn";
         divBlockSix.appendChild(selectTime);
 
             const optionA = document.createElement("option");
-            optionA.value= 20;
+            optionA.classList.add("option");
+            optionA.value= 1;
             optionA.textContent = "Turn 1: 20:00";
             selectTime.appendChild(optionA);
 
             const optionB = document.createElement("option");
-            optionB.value= 22;
+            optionB.classList.add("option");
+            optionB.value= 2;
             optionB.textContent = "Turn 2: 22:00";
             selectTime.appendChild(optionB);
 
 
-        //CONTINUE HERE NEXT TIME
+
     const divBlockSeven = document.createElement("div");
     divBlockSeven.classList.add("block");
     formContainer.appendChild(divBlockSeven);
@@ -293,36 +295,63 @@ function addPopupReservation(){
         const buttonSubmit = document.createElement("button");
         buttonSubmit.classList.add("button");
         buttonSubmit.type = "button"; //For real back end should be "submit"
-        buttonSubmit.textContent = "Submit";
+        buttonSubmit.textContent = "Submit Reservation";
         divBlockSeven.appendChild(buttonSubmit);
         buttonSubmit.addEventListener("click", () =>{
             
-            const newTitle = document.getElementById("bookTitle").value;
-            const newAuthor = document.getElementById("bookAuthor").value;
-            const newPages = document.getElementById("bookPages").value;
-            const newIsRead = document.getElementById("bookRead").checked; //if it is checked then its true its read.
-            const newImage = document.getElementById("bookImage").value;
-            const newReview = document.getElementById("bookReview").value;
-            if ( (newTitle == "") || (newAuthor == "") || (newPages == "") || (newImage == "") || (newReview == "") ) {
-                alert("Le Bom Borombom only takes fully opinionated reviews, you can't just leave blank fields, jazz it up a little deary.");
+            const newName = document.getElementById("name").value;
+            const newPhone = document.getElementById("phone").value;
+            const newMail = document.getElementById("mail").value;
+            const newGroupSize = document.getElementById("two").checked?("2"):("4") ; //if it is checked then its true its 2.
+            const newDate = document.getElementById("date").value;
+            const newTurn = document.getElementById("time").value;
+            
+            if ( (newName == "") || (newPhone == "") || (newMail == "") || (newDate == "") || (newTurn == "") ) {
+                alert("We need all fields completed to take a reservation!");
                 
             }else{
-                let newBook = new LibraryBook;
-                newBook.title = newTitle;
-                newBook.author = newAuthor;
-                newBook.pages = newPages;
-                newBook.isRead = newIsRead;
-                newBook.img = newImage;
-                newBook.review = newReview;
-                addBookToLibrary(newBook);
-                console.log(myLibrary);
-                fillCards([newBook]);
-                htmlContainer.removeChild(popup);
+                
+                let newClient = new Reservation;
+                newClient.name = newName;
+                newClient.phone = newPhone;
+                newClient.mail = newMail;
+                newClient.groupSize = newGroupSize;
+                newClient.date = newDate;
+                newClient.turn = newTurn;
+                addReservation(newClient);
+                console.table(clientReservations);
+
+                bg.removeChild(popup);
             }
             
         });
 }
 
+function Reservation(name, phone, mail, groupSize, date, turn){
+    this.name = name;
+    this.phone = phone;
+    this.mail = mail;
+    this.groupSize = groupSize;
+    this.date = date;
+    this.turn = turn;
+
+}
+
+function addReservation(client){
+    //do stuff here..
+    clientReservations.push(client);
+}
+
+let clientReservations = [
+    {
+        name:"John Placeholder",
+        phone: "74747474",
+        mail: "john@placeholder.com",
+        groupSize: "2",
+        date: "22/22/2022",
+        turn: "1"
+    },
+]
 
 
 export default addFood;
